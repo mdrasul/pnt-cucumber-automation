42553d4e34fdfeefaf0770adb159e9060d173d77
Maaria Ahmad <52634878+maaria322@users.noreply.github.com>
2019-08-26 22:47:56 -0400

    Add files via upload

---
+++ src/test/java/com/pnt/stepdefinition/AnarkaliDressXpath.java
@@ -0,0 +1,13 @@
+package xPath;
+
+import multiCartTestBase.DriverFactory;
+
+public class AnarkaliDressXpath extends DriverFactory{
+
+	public String AnarkaliShopNow =  "//*[@id=\"102\"]/div/div[1]/div/a[2]";
+	public String AnarkaliPrice= "/html/body/div[2]/div[1]/div[6]/div[6]/div/div[2]/span";
+	public String AnarkaliCircleAroundPrice= "/html/body/div[2]/div[1]/div[6]/div[6]/div/div[2]";
+	public String AnarkaliProductImage= "/html/body/div[2]/div[1]/div[6]/div[6]/div/a/div/img";
+	
+	
+}
---
+++ src/test/java/com/pnt/stepdefinition/BlackKajalXpath.java
@@ -0,0 +1,12 @@
+package xPath;
+
+import multiCartTestBase.DriverFactory;
+
+public class BlackKajalXpath extends DriverFactory {
+
+	public String BlackKajalShopNow =  "//*[@id=\"107\"]/div/div[1]/div/a[2]";
+	public String BlackKajalPrice= "/html/body/div[2]/div[1]/div[6]/div[4]/div/div[2]/span";
+	public String BlackKajalCircleAroundPrice= "/html/body/div[2]/div[1]/div[6]/div[4]/div/div[2]";
+	public String BlackKajalProductImage= "/html/body/div[2]/div[1]/div[6]/div[4]/div/a/div/img";
+
+}
