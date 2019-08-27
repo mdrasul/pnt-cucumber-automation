dc12cdd836fabe0d3843f5dbe993f11de11da139
Maaria Ahmad <52634878+maaria322@users.noreply.github.com>
2019-08-26 22:47:10 -0400

    Add files via upload

---
+++ src/test/java/com/pnt/pageobject/AnarkaliDressFunction.java
@@ -0,0 +1,27 @@
+package multiCartFunction;
+
+import org.openqa.selenium.By;
+
+import multiCartUtility.Util;
+
+public class AnarkaliDressFunction extends Util {
+
+
+
+public void AnarkaliClick() {
+
+driver.findElement(By.xpath(AnarkaliDress.AnarkaliShopNow)).click();	
+	
+	
+	
+}
+//
+//
+//public void validateProName(String desiredText) {
+//textValidation(desiredText, AnarkaliDress.AnarkaliShopNow);
+//
+//
+//}
+//
+//
+}
---
+++ src/test/java/com/pnt/pageobject/BlackKajalFunction.java
@@ -0,0 +1,26 @@
+package multiCartFunction;
+
+import org.openqa.selenium.By;
+
+import multiCartUtility.Util;
+
+
+
+public class BlackKajalFunction extends Util {
+
+
+public void BlackKajalClick() {
+
+driver.findElement(By.xpath(BlackKajal.BlackKajalShopNow)).click();	
+		
+}
+
+//
+//public void validateProName(String desiredText) {
+//textValidation(desiredText, BlackKajal.BlackKajalShopNow);
+//
+//
+//}
+
+
+}
